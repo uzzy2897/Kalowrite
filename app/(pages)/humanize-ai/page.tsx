71cbbc022b87736a4f05ba0e3f60819b0160cf67
REDA FATTAH <redafattah01@gmail.com>
2025-09-23 16:20:08 +0100

    fkkf

--- app/(pages)/humanize-ai/page.tsx
+++ app/(pages)/humanize-ai/page.tsx
@@ -8,7 +8,7 @@ import { TriangleAlert } from "lucide-react";
 
 export default async function HumanizerPage() {
   const { has } = await auth();
-  const hasPaidPlan = has({ plan:"ultra" });
+  const hasPaidPlan = has({ plan:"free" });
 
   return (
     <Suspense
@@ -18,26 +18,11 @@ export default async function HumanizerPage() {
         </main>
       }
     >
-      {hasPaidPlan ? (
+
         <HumanizerPageContent />
-      ) : (
-        <main className="flex items-center justify-center min-h-screen px-4">
-          <div className="max-w-md border p-4  flex flex-col gap-3">
-            <TriangleAlert className="h-5 w-5 text-yellow-500" />
-            <h2 className="text-lg font-bold" >Upgrade Required</h2>
-            <p className="text-muted-foreground text-sm">
-              This feature is only available on the{" "}
-              <span className="font-semibold">Ultra Plan</span>.  
-              Upgrade now to unlock full access.
-            </p>
-            <div className="mt-4 flex justify-end">
-              <Button asChild>
-                <Link href="/pricing">Upgrade Plan</Link>
-              </Button>
-            </div>
-          </div>
-        </main>
-      )}
+
+      
+     
     </Suspense>
   );
 }
