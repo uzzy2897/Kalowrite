bbd5de7b871b3aea8a524093d6012ccd9829e248
REDA FATTAH <redafattah01@gmail.com>
2025-09-24 08:52:55 +0100

    new stuff

--- app/(pages)/humanize-ai/page.tsx
+++ app/(pages)/humanize-ai/page.tsx
@@ -1,14 +1,9 @@
 import { Suspense } from "react";
 import HumanizerPageContent from "./HumanizerPageContent";
-import { auth } from "@clerk/nextjs/server";
-import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
-import { TriangleAlert } from "lucide-react";
 
 export default async function HumanizerPage() {
-  const { has } = await auth();
-  const hasPaidPlan = has({ plan:"free" });
+
 
   return (
     <Suspense
