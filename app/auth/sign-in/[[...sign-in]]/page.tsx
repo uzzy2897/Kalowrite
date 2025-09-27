716c8d61d43e647ca9819e30613042764c5dd138
REDA FATTAH <redafattah01@gmail.com>
2025-09-27 12:29:06 +0100

    lkf

--- app/auth/sign-in/[[...sign-in]]/page.tsx
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import { SignIn } from "@clerk/nextjs";
-
+import { dark } from "@clerk/themes";
 
 export default function SignInPage() {
-
-
   return (
-    <main className="flex h-screen items-center justify-center"> <SignIn
-  
-    />
-    </main>
-    
+    <div className="flex h-screen items-center justify-center">
+      <SignIn
+
+       appearance={{
+        elements:{
+          formButtonPrimary:'#2CB175',
+         
+        },
+        baseTheme: dark,
+        variables: { colorPrimary: '#2CB175', colorTextOnPrimaryBackground:'#FFFFFF', colorBackground:' #2b2b2b',colorInput:" #0C0B08" },
+      }}
+ 
+       
+      />
+    </div>
   );
 }
--- app/auth/signup/[[...sign-up]]/page.tsx
+++ app/auth/signup/[[...sign-up]]/page.tsx
@@ -1,10 +1,23 @@
 // app/sign-up/[[...sign-up]]/page.tsx
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <SignUp />
+      <SignUp 
+     
+     appearance={{
+      elements:{
+        formButtonPrimary:'#2CB175',
+       
+      },
+      baseTheme: dark,
+      variables: { colorPrimary: '#2CB175', colorTextOnPrimaryBackground:'#FFFFFF', colorBackground:' #2b2b2b',colorInput:" #0C0B08" },
+    }}
+
+     
+    />
     </main>
   );
 }
